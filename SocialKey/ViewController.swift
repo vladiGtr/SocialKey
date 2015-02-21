514802384077f6c20fc0379f85b4f6f69b137193
Vladimir Lopez <vladimirlopez@Vladimirs-MacBook-Pro.local>
2015-02-21 15:49:34 -0600

    Initial Commit

---
+++ SocialKey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialKey
+//
+//  Created by Vladimir Lopez on 2/21/15.
+//  Copyright (c) 2015 Inteligencia Móvil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
